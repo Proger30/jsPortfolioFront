588f27b936d7c1270e0b5a319f09ab2712893a35
f8b4i2 <45534457+Proger30@users.noreply.github.com>
2023-06-12 17:26:35 +0600

    Update SocialMedia.jsx

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,11 +4,11 @@ import { FaFacebookF, FaGithub  } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-	{/* <a href="https://github.com/Proger30" target="_blank" rel="noreferrer">
+	<a href="https://hh.kz/resume/7a8260d6ff0c0e53120039ed1f7a726d76504f" target="_blank" rel="noreferrer">
 		<div>
-		<BsTwitter />
-	    </div>
-	</a> */}
+			<p>hh</p>
+		</div>
+	</a>
 	<a href="https://github.com/Proger30" target="_blank" rel="noreferrer">
 		<div>
 			<FaGithub />
