240af5df7e82cfa45e25dfa1463b4091aa8f27ec
f8b4i2 <45534457+Proger30@users.noreply.github.com>
2023-06-13 15:12:09 +0600

    Update Footer.jsx

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,140 +1,129 @@
-.app__footer {
-  flex: 1;
-  width: 100%;
-  flex-direction: column;
+import React, { useState } from 'react';
 
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-  }
-}
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
+import './Footer.scss';
 
-.app__footer-cards {
-  width: 60%;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-wrap: wrap;
-  margin: 4em 2rem 2rem;
+const Footer = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  .app__footer-card {
-    min-width: 290px;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
+  const { username, email, message } = formData;
 
-    margin: 1rem 0;
-    padding: 1rem;
-    border-radius: 10px;
-    cursor: pointer;
-    background-color: #fce5ea;
-	&.phone-card {
-		background-color: #e5fcf3;
-	}
-	&.whatsapp-card {
-		background-color: #e5fce9;
-	}
-	&.telegram-card {
-		background-color: #e5e9fc;
-	}
-	&.github-card {
-		background-color: #e5e6fc;
-	}
-	&.headhunter-card {
-		background-color: #fcf6e5;
-	}
-	&.instagram-card {
-		background-color: #f9e5fc;
-	}
-	
-    transition: all 0.3s ease-in-out;
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    img {
-      width: 40px;
-      height: 40px;
-      margin: 0 0.7rem;
-    }
+  const handleSubmit = () => {
+    setLoading(true);
 
-    p {
-      font-weight: 500;
-    }
-    a {
-      text-decoration: none;
-      font-weight: 500;
-    }
-    &:hover {
-      box-shadow: 0 0 25px #fef4f5;
-    }
+    const contact = {
+      _type: 'contact',
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
 
-    @media screen and (max-width: 450px) {
-      width: 100%;
-    }
-  }
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
+  };
 
-  @media screen and (max-width: 768px) {
-    width: 100%;
-  }
-}
+  return (
+    <>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
 
-.app__footer-form {
-  width: 60%;
-  flex-direction: column;
-  margin: 1rem 2rem;
+      <div className="app__footer-cards">
+	  	
+		
+	  <a href="mailto:f8b4i2@gmail.com" className="p-text">
+			<div className="app__footer-card email-card">
+			<img src={images.email} alt="email" />
+			f8b4i2@gmail.com
+			</div>
+		</a>
+        <a href="mailto:sultanbyzholdbay@outlook.com" className="p-text">
+			<div className="app__footer-card email-card">
+          	<img src={images.email} alt="email" />
+			sultanbyzholdbay@outlook.com
+        	</div>
+		</a>
 
-  div {
-    width: 100%;
+		<a href="https://hh.kz/resume/7a8260d6ff0c0e53120039ed1f7a726d76504f" className="p-text" target="_blank" rel="noreferrer">
+			<div className="app__footer-card headhunter-card">
+			<img src={images.headHunter} alt="email" />
+			HeadHunter
+			</div>
+		</a>
+		<a href="https://github.com/Proger30" className="p-text">
+			<div className="app__footer-card github-card">
+			<img src={images.github} alt="email" />
+			GitHub
+			</div>
+		</a>	  	
+        <a href="tel:+7 (708) 499-7639" className="p-text">
+			<div className="app__footer-card phone-card">
+			<img src={images.mobile} alt="phone" />
+			+7 (708) 499-7639
+			</div>
+		</a>
+		<a href="https://wa.me/77084997639" target="_blank" rel="noreferrer" className="p-text">
+			<div className="app__footer-card whatsapp-card">
+			<img src={images.whatsapp} alt="whatsapp" />
+			+7 (708) 499-7639
+			</div>
+		</a>
+		<a href="https://www.instagram.com/s.u.1.t.a.n/" target="_blank" rel="noreferrer" className="p-text">
+			<div className="app__footer-card instagram-card">
+			<img src={images.instagram} alt="telegram" />
+			@s.u.1.t.a.n
+			</div>
+		</a>
+		<a href="https://t.me/Sultanbii" target="_blank" rel="noreferrer" className="p-text">
+			<div className="app__footer-card telegram-card">
+			<img src={images.telegram} alt="telegram" />
+			@Sultanbii
+			</div>
+		</a>
+      </div>
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank you for getting in touch!
+          </h3>
+        </div>
+      )}
+    </>
+  );
+};
 
-    margin: 0.75rem 0;
-    border-radius: 10px;
-    cursor: pointer;
-    background-color: var(--primary-color);
-
-    transition: all 0.3s ease-in-out;
-
-    input,
-    textarea {
-      width: 100%;
-      padding: 0.95rem;
-      border: none;
-      border-radius: 7px;
-      background-color: var(--primary-color);
-
-      font-family: var(--font-base);
-      color: var(--secondary-color);
-      outline: none;
-    }
-
-    textarea {
-      height: 170px;
-    }
-
-    &:hover {
-      box-shadow: 0 0 25px var(--primary-color);
-    }
-  }
-
-  button {
-    padding: 1rem 2rem;
-    border-radius: 10px;
-    border: none;
-    background-color: var(--secondary-color);
-
-    font-weight: 500;
-    color: var(--white-color);
-    outline: none;
-    margin: 2rem 0 0 0;
-    font-family: var(--font-base);
-
-    transition: cubic-bezier(0.55, 0.085, 0.68, 0.53);
-    cursor: pointer;
-
-    &:hover {
-      background-color: #2430af;
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    width: 100%;
-    margin: 1rem 0;
-  }
-}
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg',
+);
